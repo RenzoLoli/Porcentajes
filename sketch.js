47ed6d2e4ca3e52f1ee8f51699b110c4993fc73a
renzo <renzololi1@hotmail.com>
2018-12-16 01:07:35 -0500

    listo

---
+++ sketch.js
@@ -0,0 +1,111 @@
+var boton = document.getElementById("boton");
+var caja = document.getElementById("numero");
+var paridad = document.getElementById("paridad");
+var precio = 0;
+
+function generarAleatorio() {
+  
+  var probabilidad = Math.floor(Math.random() * 100);
+
+  if(probabilidad >=0 && probabilidad <= 30){
+
+    precio = generarPar();
+
+    paridad.innerHTML = "estas dentro del 30% con " + probabilidad + " :)";
+    caja.innerHTML = "precio: " + precio;
+
+  }
+  else {
+
+    precio = generarImpar();
+
+    paridad.innerHTML = "No estas dentro del 30% con " + probabilidad + " :C";
+    caja.innerHTML = "precio: " + precio;
+
+  }
+
+  colorBoton(precio);
+
+
+
+}
+
+function colorBoton(precio){
+
+  if(precio%5 == 0 && precio%10 != 0){
+
+    boton.style.backgroundColor = 'skyblue';
+    boton.style.color = 'white';
+    return;
+
+  }
+
+  if(precio >= 0 && precio <= 100){
+
+    boton.style.backgroundColor = 'blue';
+    boton.style.color = 'white';
+
+  }
+  else if(precio >= 101 && precio <= 299){
+
+    boton.style.backgroundColor = 'green';
+    boton.style.color = 'white';
+
+  }
+  else if(precio >= 300 && precio <= 500){
+
+    boton.style.backgroundColor = 'red';
+    boton.style.color = 'white';   
+
+  }
+  else if(precio >= 501 && precio <= 1000){
+
+    boton.style.backgroundColor = 'purple';
+    boton.style.color = 'white';
+
+  }
+
+}
+
+function generarPar(){
+
+  var x = 0;
+
+  do{
+
+   x = Math.floor(Math.random() * 1000);
+
+  }while(x % 2 != 0);
+
+  return x;
+
+}
+
+function generarImpar(){
+
+  var x = 0;
+
+  do{
+
+   x = Math.floor(Math.random() * 1000);
+
+  }while(x % 2 == 0);
+
+  return x;
+
+}
+
+function digitos(x){
+
+  var dig = 0;
+
+  do{
+
+    dig++;
+    x/=10;
+
+  }while(x>=1);
+
+  return dig;
+
+}
